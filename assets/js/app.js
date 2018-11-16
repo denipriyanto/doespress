8bed4ab8ee52f9299b30b52f34fa6ef7bace65be
Denipriyanto <Denipriyanto@bitbucket.org>
2018-11-16 11:14:05 +0700

    add & fix nodesass, layout dashboard

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,7 +6,10 @@
  */
 
 // any CSS you require will output into a single css file (app.css in this case)
-require('../css/app.css');
+// require('../css/app.css');
+
+// Import scss file
+import '../css/app.scss';
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
 // var $ = require('jquery');
