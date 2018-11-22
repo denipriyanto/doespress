3d69340b371d33e1971f5a58f35821185974f14e
Denipriyanto <Denipriyanto@bitbucket.org>
2018-11-22 15:01:20 +0700

    add tinymce to encore, & tinymce only on route article new

--- assets/js/tinymce.js
+++ assets/js/tinymce.js
@@ -1,1 +1,10 @@
 console.log('tinymce editor');
+
+tinymce.init({
+    selector: '#mytextarea',
+    menubar: false,
+    toolbar: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | print preview media fullpage | forecolor backcolor emoticons',
+    branding: false,
+    statusbar: false,
+    height: 600
+});
