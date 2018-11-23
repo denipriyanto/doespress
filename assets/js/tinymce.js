3d6eb7282b9398c31afac8456f29e01bfe7f38f7
Denipriyanto <Denipriyanto@bitbucket.org>
2018-11-23 16:28:34 +0700

    Bad day

--- assets/js/tinymce.js
+++ assets/js/tinymce.js
@@ -3,8 +3,15 @@ console.log('tinymce editor');
 tinymce.init({
     selector: '#mytextarea',
     menubar: false,
-    toolbar: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | print preview media fullpage | forecolor backcolor emoticons',
     branding: false,
     statusbar: false,
-    height: 600
+    height: 600,
+    image_advtab: true,
+    paste_data_images: true,
+    plugins: [
+        "advlist autolink lists link image charmap print preview hr anchor pagebreak",
+        "searchreplace wordcount visualblocks visualchars code fullscreen",
+        "insertdatetime media nonbreaking save table directionality",
+        "emoticons template paste textpattern"
+    ]
 });
